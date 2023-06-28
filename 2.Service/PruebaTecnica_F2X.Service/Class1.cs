91f53f963cd250c63b8ec37503a2887de02588a5
Gastón Alberto Cárdenas Chicangana <45832549+gacardenasch80@users.noreply.github.com>
2023-06-27 21:56:36 -0500

    Agregar archivos de proyecto.

---
+++ 2.Service/PruebaTecnica_F2X.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PruebaTecnica_F2X.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ 3.Data/PruebaTecnica_F2X.UnitOfWork/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PruebaTecnica_F2X.UnitOfWork
+{
+    public class Class1
+    {
+    }
+}
